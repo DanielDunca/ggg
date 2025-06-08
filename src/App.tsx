3eda109ef2db16b679aaa8e2c547ba6aa1f1c3b8
DanielDunca <daniel.dunca05@e-uvt.ro>
2025-06-09 01:13:08 +0300

    Initialize React project with force graph

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useMemo } from 'react';
+import data from './data/cs_map.json';
+import { Node, Link } from './types';
+import Graph from './components/Graph';
+import SearchBar from './components/SearchBar';
+import DetailsPane from './components/DetailsPane';
+
+export default function App() {
+  const [query, setQuery] = useState('');
+  const [selected, setSelected] = useState<Node | undefined>();
+
+  const nodes = useMemo(() => data.nodes as Node[], []);
+  const links = useMemo(() => data.links as Link[], []);
+
+  return (
+    <div className="flex h-screen">
+      <SearchBar query={query} onChange={setQuery} />
+      <Graph
+        nodes={nodes}
+        links={links}
+        highlight={query}
+        onNodeClick={setSelected}
+      />
+      <DetailsPane node={selected} />
+    </div>
+  );
+}
